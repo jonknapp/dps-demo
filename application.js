08ed9fa697b6f09516bef234a7267962db940bb7
Jonathan Knapp <jon@coffeeandcode.com>
2013-05-31 12:00:52 -0400

    Added ability to dump the adobe log, dump most recent magazine info, and try to download most recent magazine

--- application.js
+++ application.js
@@ -1,4 +1,6 @@
 (function(window) {
+    var folioToDownload;
+
     function error(message) {
         log('<em>ERROR:</em> ' + message);
     }
@@ -22,6 +24,69 @@
 
         $('form').on('submit', login);
         $('form [type=submit]').on('click', login);
+        $('#download').on('click', download);
+        $('#magazine-info').on('click', prepareDownload);
+        $('#print-log').on('click', dumpLog);
+    }
+
+    function download(event) {
+        event.preventDefault();
+
+        if (folioToDownload && folioToDownload.isDownloadable) {
+            try {
+                log('starting download');
+                var transaction = folioToDownload.download();
+                log('> Download Transaction id: ' + transaction.id);
+                log('> step: ' + transaction.step);
+                log('> state: ' + transaction.state);
+                log('> progress: ' + transaction.progress);
+                log('> error: ' + transaction.error);
+                log('> isCancelable: ' + transaction.isCancelable);
+                log('> isPausable: ' + transaction.isPausable);
+                log('> isDeterminate: ' + transaction.isDeterminate);
+                log('> isFailureTerminal: ' + transaction.isFailureTerminal);
+
+                transaction.completedSignal.addOnce(function() {
+                    log('download completed');
+                });
+                transaction.progressSignal.addOnce(function() {
+                    log('download progress');
+                });
+                transaction.stateChangedSignal.addOnce(function() {
+                    log('state changed of folio being downloaded');
+                });
+            } catch (error) {
+                log(error);
+            }
+        }
+    }
+
+    function dumpLog(event) {
+        event.preventDefault();
+
+        var logLevel = _.reduce($('[name="logLevels[]"]').val(), function(memo, value) {
+            return memo | adobeDPS.log.logLevels[value];
+        }, 0);
+        var logDump = adobeDPS.log.print(logLevel, true);
+        var logEntries = logDump.split("\n");
+
+        _.each(logEntries, function(value) {
+            if (value !== '') {
+                log(value);
+            }
+        });
+    }
+
+    function folios() {
+        return adobeDPS.libraryService.folioMap.sort(function (a, b) {
+            if(a.publicationDate < b.publicationDate) {
+                return 1;
+            } else if(a.publicationDate > b.publicationDate) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
     }
 
     function login(event) {
@@ -61,6 +126,32 @@
         log('isUserAuthenticated: ' + ((adobeDPS.authenticationService.isUserAuthenticated) ? 'true' : 'false'));
         log('userName: ' + adobeDPS.authenticationService.userName);
         log('token: ' + adobeDPS.authenticationService.token);
+    }
+
+    function prepareDownload(event) {
+        event.preventDefault();
+        if (!folioToDownload) {
+            folioToDownload = folios()[0];
+
+            folioToDownload.updatedSignal.add(function(properties) {
+                log('updating ' + properties.length + ' properties');
+                for (var i = 0; i < properties.length; i++) {
+                    log('  > ' + properties[i] + ' to ' + this[properties[i]]);
+                }
+            }, folioToDownload);
+        }
+
+        log(folioToDownload.id);
+        log('current transactions: ' + folioToDownload.currentTransactions.length);
+        log('download size: ' + folioToDownload.downloadSize);
+        log('state: ' + folioToDownload.state);
+        log('preview image url: ' + folioToDownload.previewImageURL);
+        log('title: ' + folioToDownload.title);
+        log('isCompatible: ' + folioToDownload.isCompatible);
+        log('isDownloadable: ' + folioToDownload.isDownloadable);
+        log('isViewable: ' + folioToDownload.isViewable);
+        log('isArchivable: ' + folioToDownload.isArchivable);
+        log('isUpdatable: ' + folioToDownload.isUpdatable);
     }
 
     $(document).ready(function() {
